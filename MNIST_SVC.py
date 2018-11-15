92aa4d01963c924bac9c1085267100515918e463
Matthew T <mtroglia@hawaii.edu>
2018-11-14 20:51:24 -1000

    svc script update to train on lower number of samples. Runtime too long with 60000

--- MNIST_SVC.py
+++ MNIST_SVC.py
@@ -1,7 +1,7 @@
 from sklearn.svm import SVC
 from mnist import MNIST
-
-
+from sklearn.metrics import accuracy_score
+import numpy as np
 '''Example of format
 
 
@@ -10,29 +10,46 @@ model = SVC(C=1.0, kernel=’rbf’, degree=3, gamma=’auto_deprecated’, coef
 model.fit(X,Y)
 y_hat = model.predict(X_test)
 '''
-
+#%%
 
 mndataSet = '.\Dataset'
 mndata = MNIST()
 #mndata.gz =True
 mndata = MNIST(mndataSet)
 #%%importing data into lists and labels
+print("loading images....")
 images,labels = mndata.load_training()
 images_test, labels_test = mndata.load_testing()
 #%%
+print("Partition train")
+xtrain = images[0:10000]
+xlabels = labels[0:10000]
+print('building....')
 c = [.25,.5,1,2,4]
 model = SVC(C = 1)
-model.fit(images, labels)
-y_hat = model.predict(images)
+model.fit(xtrain, xlabels)
+y_hat = model.predict(xtrain)
 
+#%%
+accuracy  = accuracy_score(np.array(xlabels),y_hat)
+
+print("Training accuracy ",accuracy)
+'''
 misClass = 0
-for x in range(0,45):#len(y_hat)):
+for x in range(0,len(y_hat)):
     if x%15 == 0:
         print(y_hat[x])
-        print(labels[x])
-    if y_hat[x] == labels[x]:
+        print(xlabels[x])
+    if y_hat[x] == xlabels[x]:
         print('ok')
     else:
         misClass = misClass+1
 
-train_error = misClass/45
+train_error = misClass/len(y_hat)
+print(train_error)
+'''
+
+#%%
+y_hat_test =  model.predict(images_test)
+accuracyTest= accuracy_score(np.array(labels_test), y_hat_test)
+print("Test accuracy " , accuracyTest)
