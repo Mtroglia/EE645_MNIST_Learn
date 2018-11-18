7900a4efe01219588263bc6ccf07b23cee0ecbd5
Matthew T <mtroglia@hawaii.edu>
2018-11-17 14:32:09 -1000

    Updates

--- MNIST_SVC.py
+++ MNIST_SVC.py
@@ -22,11 +22,11 @@ images,labels = mndata.load_training()
 images_test, labels_test = mndata.load_testing()
 #%%
 print("Partition train")
-xtrain = images[0:10000]
-xlabels = labels[0:10000]
+xtrain = images#[0:10000]
+xlabels = labels#[0:10000]
 print('building....')
 c = [.25,.5,1,2,4]
-model = SVC(C = 1, kernel = 'linear')
+model = SVC(C = 5,gamma = 0.05, kernel = 'linear')
 model.fit(xtrain, xlabels)
 y_hat = model.predict(xtrain)
 
@@ -50,6 +50,7 @@ print(train_error)
 '''
 
 #%%
+print("Running Test...")
 y_hat_test =  model.predict(images_test)
 accuracyTest= accuracy_score(np.array(labels_test), y_hat_test)
 print("Test accuracy " , accuracyTest)
